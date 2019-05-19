1346f359beabcb5eef14c8dc10f6f6095176af88
SuhoSon <ngeol564@gmail.com>
2019-05-19 11:33:22 +0900

    Refactoring too complex methods

--- src/main/java/com/jagrosh/jmusicbot/commands/MusicCommand.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/MusicCommand.java
@@ -16,10 +16,13 @@
 package com.jagrosh.jmusicbot.commands;
 
 import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.settings.Settings;
 import com.jagrosh.jmusicbot.audio.AudioHandler;
+
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.GuildVoiceState;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.VoiceChannel;
@@ -45,50 +48,61 @@ public abstract class MusicCommand extends Command
     @Override
     protected void execute(CommandEvent event) 
     {
-        Settings settings = event.getClient().getSettingsFor(event.getGuild());
-        TextChannel tchannel = settings.getTextChannel(event.getGuild());
+    	Guild eventGuild = event.getGuild();
+    	CommandClient eventClient = event.getClient();
+        Settings settings = eventClient.getSettingsFor(eventGuild);
+        TextChannel tchannel = settings.getTextChannel(eventGuild);
+        
         if(tchannel!=null && !event.getTextChannel().equals(tchannel))
         {
             try 
             {
                 event.getMessage().delete().queue();
             } catch(PermissionException ignore){}
-            event.replyInDm(event.getClient().getError()+" You can only use that command in "+tchannel.getAsMention()+"!");
+            event.replyInDm(eventClient.getError()+" You can only use that command in "+tchannel.getAsMention()+"!");
             return;
         }
-        bot.getPlayerManager().setUpHandler(event.getGuild()); // no point constantly checking for this later
-        if(bePlaying && !((AudioHandler)event.getGuild().getAudioManager().getSendingHandler()).isMusicPlaying(event.getJDA()))
+        
+        bot.getPlayerManager().setUpHandler(eventGuild); // no point constantly checking for this later
+        if(bePlaying && !((AudioHandler)eventGuild.getAudioManager().getSendingHandler()).isMusicPlaying(event.getJDA()))
         {
-            event.reply(event.getClient().getError()+" There must be music playing to use that!");
+            event.reply(eventClient.getError()+" There must be music playing to use that!");
             return;
         }
+        
         if(beListening)
         {
-            VoiceChannel current = event.getGuild().getSelfMember().getVoiceState().getChannel();
+            VoiceChannel current = eventGuild.getSelfMember().getVoiceState().getChannel();
             if(current==null)
-                current = settings.getVoiceChannel(event.getGuild());
-            GuildVoiceState userState = event.getMember().getVoiceState();
-            if(!userState.inVoiceChannel() || userState.isDeafened() || (current!=null && !userState.getChannel().equals(current)))
-            {
-                event.replyError("You must be listening in "+(current==null ? "a voice channel" : "**"+current.getName()+"**")+" to use that!");
-                return;
-            }
-            if(!event.getGuild().getSelfMember().getVoiceState().inVoiceChannel())
-            {
-                try 
-                {
-                    event.getGuild().getAudioManager().openAudioConnection(userState.getChannel());
-                }
-                catch(PermissionException ex) 
-                {
-                    event.reply(event.getClient().getError()+" I am unable to connect to **"+userState.getChannel().getName()+"**!");
-                    return;
-                }
-            }
+                current = settings.getVoiceChannel(eventGuild);
+            if(isInvalidUserStateForAudio(event, current))
+            	return;
         }
         
         doCommand(event);
     }
+
+	private boolean isInvalidUserStateForAudio(CommandEvent event, VoiceChannel current) {
+		GuildVoiceState userState = event.getMember().getVoiceState();
+		if(!userState.inVoiceChannel() || userState.isDeafened() || (current!=null && !userState.getChannel().equals(current)))
+		{
+		    event.replyError("You must be listening in "+(current==null ? "a voice channel" : "**"+current.getName()+"**")+" to use that!");
+		    return true;
+		}
+		if(!event.getGuild().getSelfMember().getVoiceState().inVoiceChannel())
+		{
+		    try 
+		    {
+		    	event.getGuild().getAudioManager().openAudioConnection(userState.getChannel());
+		    }
+		    catch(PermissionException ex) 
+		    {
+		        event.reply(event.getClient().getError()+" I am unable to connect to **"+userState.getChannel().getName()+"**!");
+		        return true;
+		    }
+		}
+		return false;
+	}
     
     public abstract void doCommand(CommandEvent event);
 }
